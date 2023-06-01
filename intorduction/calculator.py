2b41573332d86d4b3a855b832b63dd9c5ff09e54
Francis James <francisjames2001@gmail.com>
2023-06-01 16:53:22 -0400

    trying typecast

---
+++ intorduction/calculator.py
@@ -0,0 +1,13 @@
+""" we try to add two number from users side 
+because we know input() work as a string so 
+we are try to check by using integer """
+
+print("give a number ")
+
+num1 = int(input())
+
+print("give another number ")
+
+num2 = int(input())
+
+print( "the sum of both numbers " ,num1 + num2)
