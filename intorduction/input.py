fe15d951af051feb8dd2f8604591df134ef68fad
Francis James <francisjames2001@gmail.com>
2023-06-01 16:15:18 -0400

    take input for name and age

--- intorduction/input.py
+++ intorduction/input.py
@@ -1,7 +1,19 @@
-""" write a program in which you take input from use """
+""" write a program in which you take input from use 
 
 print("hello give some text ")
 
 text = input()
 
-print("here your words   " , text )
+
+print("here your words   " , text ) """
+
+print("hey please write your name ")
+
+name = input()
+
+print("and your age also")
+
+age = input()
+
+print("your name is ", name , "and your age is ", age )
+
