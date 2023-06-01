a052cc7b7e5e813598fd149e626e1ac5bfaec79b
Francis James <francisjames2001@gmail.com>
2023-06-01 16:05:06 -0400

    a new file for input

---
+++ intorduction/input.py
@@ -0,0 +1,7 @@
+""" write a program in which you take input from use """
+
+print("hello give some text ")
+
+text = input()
+
+print("here your words   " , text )
--- intorduction/intro.py
+++ intorduction/intro.py
@@ -24,3 +24,11 @@ multiy line
 
 comment 
 """
+
+""" write a program in which you take input from use """
+
+print("hello give some text ")
+
+text = input()
+
+print("here your words   " , text )
