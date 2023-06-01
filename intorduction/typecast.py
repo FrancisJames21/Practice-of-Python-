8c17f064fe84b813b988c830c62d2c20097ff049
Francis James <francisjames2001@gmail.com>
2023-06-01 16:29:09 -0400

    typecasting

---
+++ intorduction/typecast.py
@@ -0,0 +1,13 @@
+""" 
+IN PYTHION THERE IS A WAY TO CHANGE A FORM OF DATA TYPE IN SIMPLE
+WORDS WE CAN CHANGE DATA TYPE 
+
+"""
+a = "5"
+print( "before",type(a))
+
+a = float (a)
+print("after ", type(a))
+
+a = int (a)
+print("after ", type(a))
