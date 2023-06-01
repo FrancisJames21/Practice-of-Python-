271495aadf544fa6a0704e0289c3208900590e96
Francis James <francisjames2001@gmail.com>
2023-06-01 15:48:19 -0400

    just a starting

---
+++ intorduction/intro.py
@@ -0,0 +1,26 @@
+print("heollo james ")
+print("hey james lets do something with python ")
+
+a = 2
+print("the value of a is ", a)
+b = 2
+print("the value of b is ", b)
+c = a + b 
+print("the value of c is ", c)
+
+print(type(c))
+
+name = "francis james "
+
+print(name)
+
+print(type(name))
+
+#its all for the practice only  
+
+"""
+for 
+multiy line 
+
+comment 
+"""
