fe44c1ddf313dfd86984db439c2d825afe034da5
Francis James <francisjames2001@gmail.com>
2023-06-01 23:07:20 -0400

    trying to calculate some digit

--- intorduction/calculator.py
+++ intorduction/calculator.py
@@ -2,7 +2,7 @@
 because we know input() work as a string so 
 we are try to check by using integer """
 
-print("give a number ")
+"""print("give a number ")
 
 num1 = int(input())
 
@@ -10,4 +10,14 @@ print("give another number ")
 
 num2 = int(input())
 
-print( "the sum of both numbers " ,num1 + num2)
+print( "the sum of both numbers " ,num1 + num2) """
+
+print("Enter a number 1")
+
+num1 = int(input())
+
+print("Enter a number 2")
+
+num2 = int(input())
+
+print ("The sum is ",num1 / num2)
