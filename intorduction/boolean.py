5b6c5c856c3554da5fe5c7fb8e3b1b2e4b58cd24
Francis James <francisjames2001@gmail.com>
2023-06-02 12:26:40 -0400

    GATES

---
+++ intorduction/boolean.py
@@ -0,0 +1,76 @@
+"""a =1 
+b = 2
+print (a == b )
+
+
+a = 3 
+b = 3 
+
+print (a == b )
+
+a = 3 
+b = 3 
+c = 1
+print (a == b + c)
+
+a = 4
+b = 3 
+c = 1
+print (a == b + c ) 
+
+
+a = "francis"
+b = "francis"
+
+print (a == b )
+
+there are some GATES 
+AND  - and 
+OR  - or 
+NOT - not
+
+=> and Gate 
+
+if both are ture than its true otherwise False
+
+
+
+a = 2
+b = 4
+
+print (a ==2 and b == 4) # true
+
+a = 2
+b = 5
+
+print (a ==2 and b == 4) # false
+
+=================================================== 
+
+or Gate 
+
+if any condition is true so or will be true else it will be false
+
+a = 3
+b= 4
+
+print(a == 3 or b ==6) # true 
+
+
+a = 2
+b= 4
+
+print(a == 3 or b ==6) # false
+
+======================================================
+
+ IN PYTHON NOT (!) WORK AS(not)
+
+ * not convert the condition if a condition is true than its like
+
+ not true and not false or true  
+false and true or true
+false or true
+ ans - true 
+
+"""
