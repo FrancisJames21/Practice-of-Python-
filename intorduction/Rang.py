145c81a31174e1d427d26cd649c17295f9906641
Francis James <francisjames2001@gmail.com>
2023-06-03 16:03:38 -0400

    loops

---
+++ intorduction/Rang.py
@@ -0,0 +1,83 @@
+"""
+example for range 
+
+list (range(2,20,2)) 
+
+for i in range( 10):
+    print("francis james ",i) 
+
+
+for i in range (0,50,2): # to print even number from 0 to 50
+    print(i)
+     
+
+
+for i in range (0,101,2):
+    print(i)
+=========================================================
+
+# solved a normal questions 
+
+num = int (input("Enter a number "))
+
+for num in range (1,num+1 ):
+    if(num % 3 == 0 and num % 5 == 0):
+        print("fizzbuzz", num)
+    
+    elif(num % 3 ==0):
+        print("fizz",num)
+
+    elif(num % 5 == 0):
+        print("fuzz",num)
+    
+    else:
+        print("normal" ,num) 
+
+===================================================
+
+# practice to fill a new line in a existing line 
+
+print("hello", end=" james ")
+print("francis")
+
+
+#========================================================
+
+# nested loop of for 
+
+for num in range (4):
+    print(num)
+    for ber in range(4):
+        print(ber)
+        for sss in range(4):
+            print(sss)
+
+
+number = int (input( "enter a number")) 
+
+for i in range (1,11 ):
+    print(number , "x", i,"=",number *i) """
+
+
+for i in range (1,21):
+    print("===========================")
+    for j in range (1,11):
+        print(i , "x", j, "=", i * j )
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
