7c39093cdd31a18cdc5e2314c652ca3609e005bb
Francis James <francisjames2001@gmail.com>
2023-06-12 13:18:24 -0400

    some more

---
+++ intorduction/Dictnory.py
@@ -0,0 +1,70 @@
+"""
+name = {
+
+"breakfast" : 9 ,
+"lunch" : 2,
+"Dinner" : 7
+}
+
+print(name["lunch"])"""
+
+
+"""name  ["num"]= 9 # if key is not present in dict so it will create it
+name ["lunch"]= 3 # this is how you can chnage the present value the key
+
+name.pop("num") # it will help to delete a key with its value
+
+print(name.keys())# it can print all keys 
+
+print(name.values())# it will print all values
+
+print(len(name))# it shows us the length of dict.."""
+
+"""for key in name:
+
+    print("keys are",key,"=","values are",name[key])# we can use it in for loop also"""
+
+
+# in - key is alos use in dict 
+
+"""A = [ 1,2,3,4,45,5,56,667,7]
+
+max=A[0]
+
+for i in range (1,len(A)):
+
+    if(A[i]>max):
+
+        max = A[i]
+
+print(max)
+"""
+
+A= {
+    "francis" : 21,
+    "shiny" : 20,
+    "james" : 32,
+    "kajal" : 45,
+    "renu" : 50
+
+    
+    }
+
+print(A["francis"])
+
+A ["name"] = [11,2,2,]
+
+print(A)
+
+A["francis"]="jamesss"
+
+print(A)
+
+A.pop("renu")
+
+print(A)
+
+print(A.keys())
+
+print(A.values())
+
---
+++ practice of questions/solve 500questions_in_month.py
@@ -0,0 +1,63 @@
+
+"""500 questions in a month however 
+All questions will be solved in Python only may be some question solved by seeing them but we will solve it again without
+seeing them"""
+
+# 1 . how to print each word 3 time from a string
+
+
+"""def name (s , n ):
+
+    res = ""
+
+    for c in s :
+
+        res += c * n
+    
+    return res
+
+print(name("ABC", 4)) """
+
+##=================================================================
+
+# 2. another way to approach the same question
+
+"""s = "ABC"
+
+n = 5
+
+res = ""
+
+for c in s: # to access every character of the strig
+
+    res += c *n
+
+print(res)"""
+
+##==========================================================================
+
+# convert a string into a list or array
+
+"""words ="hey boy are you okay"
+
+word=""
+
+res = []
+
+for c in words:
+
+    if (c == " "):
+
+        res.append(word)
+
+        word =""
+    
+    else:
+        word += c
+    
+res.append(word) # append is ude to add a word in the end of the list
+    
+print(res)"""
+
+#==============================================================================
+
